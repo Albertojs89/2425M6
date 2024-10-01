25d23e09627db5858629d1af767a05d9bd266860
Albertojs89 <156692647+Albertojs89@users.noreply.github.com>
2024-10-01 18:53:32 +0000

    borrarColores

--- ejercicios/ejercicio3/index.js
+++ ejercicios/ejercicio3/index.js
@@ -48,35 +48,64 @@ let quiz=[
 let currentPosition=0;
 let successes=0;
 let failures=0;
-let forward;
+let forward=false;
+let numAleatorio;
 
-
+ const casillas=document.querySelectorAll('.div')
 
 
 // MOVER PERSONAJE---------------------------------------------------------------------
 function movePlayer(forward){
-    
-   
-}
+    // console.log('forward', forward)
 
-// GENERAR PREGUNTA-----------------------------------------------------------------
-function generarPregunta(){
     if (forward===true){
         currentPosition++;
         successes++;
+        casillas[currentPosition].classList.add('div-point')
         
     }else if(forward===false){
         currentPosition=currentPosition-3;
         failures++;
+        if(currentPosition<1){
+            currentPosition=1;
+        }
+        casillas[currentPosition].classList.remove('div-point')
+        casillas[currentPosition].classList.add('div')
+        
+        
         
     }
-    let numAleatorio=Math.floor(Math.random()*quiz.length)
+}
+
+// GENERAR PREGUNTA-----------------------------------------------------------------
+function generarPregunta(){
+    
+    numAleatorio=Math.floor(Math.random()*quiz.length)
     document.querySelector("#question").innerHTML=`${quiz[numAleatorio].question}`;
 
 
     document.querySelector("#answer1").innerHTML=`${quiz[numAleatorio].answers[0].text}`
+    document.querySelector("#answer2").innerHTML=`${quiz[numAleatorio].answers[1].text}`
+    document.querySelector("#answer3").innerHTML=`${quiz[numAleatorio].answers[2].text}`
+    document.querySelector("#answer4").innerHTML=`${quiz[numAleatorio].answers[3].text}`
+
+
+
+}
+
+function comprobarFinal(){
+    if(currentPosition==20){
+        console.log('HAS LLEGADO A LA CASILLA 20, FELICIDADES!')
+        document.querySelector("#result").innerHTML="HAS GANADO"
+        const botonOff=document.querySelector("#next-question");
+        botonOff.disabled=true;
+    }
+}
     
-    document.querySelector("#answer1").addEventListener('click',function(){
+generarPregunta();
+
+document.querySelector("#answer1").addEventListener('click',function(){
+    
     if (quiz[numAleatorio].answers[0].correct==true){
         document.querySelector("#result").innerHTML="Correcto"
         forward=true
@@ -86,47 +115,49 @@ function generarPregunta(){
         forward=false
         movePlayer(forward);
     }
-    })
-    document.querySelector("#answer2").innerHTML=`${quiz[numAleatorio].answers[1].text}`
-        document.querySelector("#answer2").addEventListener('click',function(){
-            if (quiz[numAleatorio].answers[1].correct==true){
-                document.querySelector("#result").innerHTML="Correcto"
-                forward=true
-                movePlayer(forward);
-            }else{
-                document.querySelector("#result").innerHTML="Incorrecto"
-                forward=false
-                movePlayer(forward);
-            }
-        })
-    document.querySelector("#answer3").innerHTML=`${quiz[numAleatorio].answers[2].text}`
-        document.querySelector("#answer3").addEventListener('click',function(){
-            if (quiz[numAleatorio].answers[2].correct==true){
-                document.querySelector("#result").innerHTML="Correcto"
-                forward=true
-                movePlayer(forward);
-            }else{
-                document.querySelector("#result").innerHTML="Incorrecto"
-                forward=false
-                movePlayer(forward);
-            }
-        })
-    document.querySelector("#answer4").innerHTML=`${quiz[numAleatorio].answers[3].text}`
-        document.querySelector("#answer4").addEventListener('click',function(){
-            if (quiz[numAleatorio].answers[3].correct==true){
-                document.querySelector("#result").innerHTML="Correcto"
-                forward=true
-                movePlayer(forward);
-            }else{
-                document.querySelector("#result").innerHTML="Incorrecto"
-                forward=false
-                movePlayer(forward);
-            }
-        })
-}
+})
+    
+        
+document.querySelector("#answer2").addEventListener('click',function(){
+        if (quiz[numAleatorio].answers[1].correct==true){
+            document.querySelector("#result").innerHTML="Correcto"
+            forward=true
+            movePlayer(forward);
+        }else{
+            document.querySelector("#result").innerHTML="Incorrecto"
+            forward=false
+            movePlayer(forward);
+        }
+})
+    
+        
+document.querySelector("#answer3").addEventListener('click',function(){
+        if (quiz[numAleatorio].answers[2].correct==true){
+            document.querySelector("#result").innerHTML="Correcto"
+            forward=true
+            movePlayer(forward);
+        }else{
+            document.querySelector("#result").innerHTML="Incorrecto"
+            forward=false
+            movePlayer(forward);
+        }
+})
+    
+document.querySelector("#answer4").addEventListener('click',function(){
+        if (quiz[numAleatorio].answers[3].correct==true){
+            document.querySelector("#result").innerHTML="Correcto"
+            forward=true
+            movePlayer(forward);
+        }else{
+            document.querySelector("#result").innerHTML="Incorrecto"
+            forward=false
+            movePlayer(forward);
+        }
+})
 
 
-generarPregunta();
+
+
 
 
 
@@ -138,9 +169,15 @@ generarPregunta();
 //reiniciar el boton siguiente -----------
 
 document.querySelector("#next-question").addEventListener('click',function(){
-    // window.location.href="index.html";  no valida, recarga de 0
+    //Control de error: La posición no baja del 1
+    if(currentPosition<1){
+        currentPosition=1;
+    }
+    //Borramos el resultado para que no se vea nada
     document.querySelector("#result").innerHTML="";
+    //Volvemos a llamar a la funcion generarPregunta y comprobamos si hemos ganado:
     generarPregunta()
+    comprobarFinal()
 
     console.log("posicion actual: ",currentPosition);
     console.log("aciertos: ",successes);
