5583627e37432977557bbb0a30537e22c352d701
Albertojs89 <156692647+Albertojs89@users.noreply.github.com>
2024-11-04 11:45:51 +0000

    prac06

--- ejercicios/ejercicio6/index.js
+++ ejercicios/ejercicio6/index.js
@@ -1,7 +1,5 @@
-console.log("Hola")
+console.log("Hola");
 
-
-  
 /* EXPLICACION MATHRANDOM ENTRE DOS VALORES MAX Y MIN----------
 
 -La funcion coje un numero minimo y maximo
@@ -12,52 +10,77 @@ console.log("Hola")
 
 */
 
-
 function numeroAleatorio(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-  // const numero = numeroAleatorio(1, 100);
-  // console.log(numero); 
+// const numero = numeroAleatorio(1, 100);
+// console.log(numero);
 
- document.querySelector("#btnEnviar").addEventListener('click',function(){
-  let minimo=parseInt(document.querySelector("#num1").value);
-  let maximo=parseInt(document.querySelector("#num2").value);
-  
-  let numAleatorio=numeroAleatorio(minimo,maximo);
-  
-  document.querySelector("#numeroAleatorio").innerHTML=`El numero aleatorio es ${numAleatorio}`
+document.querySelector("#btnEnviar").addEventListener("click", function () {
+  let minimo = parseInt(document.querySelector("#num1").value);
+  let maximo = parseInt(document.querySelector("#num2").value);
 
+  let numAleatorio = numeroAleatorio(minimo, maximo);
 
-})
-
+  document.querySelector(
+    "#numeroAleatorio"
+  ).innerHTML = `El numero aleatorio es ${numAleatorio}`;
+});
 
 //Ejercicio 2 convertir a Mayusculas y minisculas---------------------
 
-let txtAnalizar=document.querySelector("#texto");
+let txtAnalizar = document.querySelector("#texto");
 
-document.querySelector("#btnMayus").addEventListener('click',function(){
-  let txtMayus=txtAnalizar.value.toUpperCase();
-  document.querySelector("#texto").value=txtMayus;
+document.querySelector("#btnMayus").addEventListener("click", function () {
+  let txtMayus = txtAnalizar.value.toUpperCase();
+  document.querySelector("#texto").value = txtMayus;
 });
 
-document.querySelector("#btnMinus").addEventListener('click',function(){
-  let txtMinus=txtAnalizar.value.toLowerCase();
-  document.querySelector("#texto").value=txtMinus;
+document.querySelector("#btnMinus").addEventListener("click", function () {
+  let txtMinus = txtAnalizar.value.toLowerCase();
+  document.querySelector("#texto").value = txtMinus;
 });
-
 
 //BUSCADOR DE PALABRAS EN UN TEXTO-----------------------------------
 
+document.querySelector("#btnBuscar").addEventListener("click", function () {
+  let palabraBusqueda = document.querySelector("#busqueda").value.toLowerCase(); // Convertimos a minúsculas para una comparación más robusta
+  let textoBuscar = document
+    .querySelector("#textoBuscar")
+    .textContent.toLowerCase(); // Obtenemos el texto del span y lo pasamos a minúsculas
+  let palabras = textoBuscar.split(" ");
+  let nuevoTexto = "";
+  let contador = 0;
 
+  for (let i = 0; i < palabras.length; i++) {
+    if (palabras[i] === palabraBusqueda) {
+      //remarcar la palabra repetida
+      nuevoTexto += `<span class="bg-primary text-light">"${palabras[i]}"</span> `;
+      //modificar la palabra con element.textContent:
 
-document.querySelector("#btnBuscar").addEventListener('click',function(){
-  let palabraBusqueda=document.querySelector("#busqueda").value;
-  // console.log(palabraBusqueda);
-  
+      contador++;
+    } else {
+      nuevoTexto += palabras[i] + " ";
+    }
+  }
 
+  // Asignamos el nuevo texto con las palabras resaltadas al elemento HTML
+  document.querySelector("#textoBuscar").innerHTML = nuevoTexto.trim();
+  //trim(): Es un método en JavaScript que se aplica a cadenas de texto (strings).
+  //Su función principal es eliminar los espacios en blanco al inicio y al final de una cadena.
+  //Estos espacios en blanco pueden ser espacios, tabulaciones o saltos de línea.
+  document.querySelector(
+    "#contadorPalabras"
+  ).innerHTML = `La palabra "${palabraBusqueda}" se repite ${contador} veces`;
 
+  //Resumen de 10 palabras
+
+  function obtenirSubstr(palabras) {
+    const primeras10 = palabras.slice(0, 10);
+    console.log(primeras10);
+  }
+
+  // Exemple d'ús:
+  document.querySelector("#resumen").innerHTML = obtenirSubstr(palabras).trim();
 });
-
-
-
